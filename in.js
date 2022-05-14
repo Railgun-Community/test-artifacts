824d2678246e4bf1cbc0d477360ae9c34eccbe96
Hisham Galal <0xhgalal@gmail.com>
2022-05-15 01:55:37 +0200

    remove test file

--- in.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const compress = require('brotli/compress');
-const path= ['1-2', '1-3', '2-2', '2-3', '8-2'];
-for(let i=0; i<path.length; i++){
-  const pathIn = `${__dirname}/${path[i]}/native.dat`;
-  const pathOut = `${__dirname}/${path[i]}/native.br`
-  fs.writeFileSync(pathOut, compress(fs.readFileSync(pathIn)))
-}
